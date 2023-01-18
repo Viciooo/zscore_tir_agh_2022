6293417e95f26b8253056d90556fc43c95738e5b
Piotr Witek <72527323+Viciooo@users.noreply.github.com>
2023-01-18 21:08:51 +0100

    Improved data generation
    Points are now generated using normal (Gaussian) distribution

--- src/data_generation.py
+++ src/data_generation.py
@@ -1,9 +1,6 @@
-import random
+import numpy as np
+arr = np.random.normal(loc=0.0, scale=10, size=[1000,3])
 
-def random_point():
-    return f"{random.uniform(0, 10.0)},{random.uniform(0, 10.0)},{random.uniform(0, 10.0)}\n"
-
-f = open('data.csv', "a")
-for i in range(1000):
-    f.write(random_point())
-f.close()
+with open('data.csv', "a") as f:
+    for row in arr:
+        f.write(f"{row[0]},{row[1]},{row[2]}\n")
